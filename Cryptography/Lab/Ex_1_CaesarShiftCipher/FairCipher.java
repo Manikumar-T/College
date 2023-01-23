195e903afd56344b526c360ce8a31d6fd6e2a496
Manikumar <kumarmani862003@gmail.com>
2023-01-23 20:31:40 +0530

    after complete the test Preprocessing

--- Cryptography/Lab/Ex_1_CaesarShiftCipher/FairCipher.java
+++ Cryptography/Lab/Ex_1_CaesarShiftCipher/FairCipher.java
@@ -1,19 +1,36 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class FairCipher {
+    
     public static void main(String[] args) {
-        String msg = "today",preprotext = "";
-        char prv,cor;int i=0;
-        while (i<msg.length()) {
-            cor = msg.charAt(i);
-            prv = msg.charAt(i+1);
-            if(cor == prv) {
-                preprotext=cor+"x_";
-                i+=1;    
-            }else{
-                preprotext=cor+(prv+"_");
+        ArrayList<String>preprotext = new ArrayList<String>();
+        String msg = "tree",temp= "";
+        char prev,cor;int i=1;
+        while( i < msg.length()) {
+            temp+=prev=msg.charAt(i-1);
+            cor=msg.charAt(i);
+            //System.out.println("prev: "+prev+" cor:"+cor);
+            if(prev == cor) {
+                preprotext.add(temp+"x");
+                //System.out.println(" "+i);
+                if(i == msg.length()-1) {
+                    preprotext.add(msg.charAt(i)+"x");
+            }
+                i++;
+            }else {
+                preprotext.add(temp+cor);
+                if(i == msg.length()-2) {
+                        preprotext.add(msg.charAt(i+1)+"x");
+                }
                 i+=2;
             }
-            System.out.println(preprotext);
 
+            
+            temp="";
+      
         }
+        
+        System.out.println("Pre Processed text: "+Arrays.toString(preprotext.toArray()));
     }
 }
