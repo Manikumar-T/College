062043942899037dfedd06bde45d68481955cf07
Manikumar-T <kumarmani862003@gmail.com>
2023-04-07 14:31:14 +0530

    so far working for default inputs but not work of custom input

--- Cryptography/Lab/Ex6_HillCipher/HillCipher.java
+++ Cryptography/Lab/Ex6_HillCipher/HillCipher.java
@@ -1,6 +1,17 @@
 import java.util.*;
 public class HillCipher {
-    
+    public static void printbanner() {
+        System.out.println("'"
+        + "*******************************************************\n" +
+"     IT1681 - Cryptography and  Networks Security Laboratory\n" +
+"\n" +
+"	Roll Number : 20UIT020\n" +
+"	Name        :T.MANIKUMAR\n" +
+"	Ex. No.	    : 03\n" +
+"	Ex. Name    : Implementation of Vigenere Cipher	\n" +
+"*******************************************************\n" +
+"\n");
+    }
     static int keymatrix[][],inversmatrix[][],msgmatrix[][],n,resultmatrix[][];
     static String key,msg;
     public static void kInvers() {
@@ -54,7 +65,7 @@ public class HillCipher {
     public static void genMsgMatrix() {
         msg=msg.toLowerCase();
         int index=0;
-        System.out.println("row:"+msgmatrix.length+"column:"+msgmatrix[0].length);
+        //System.out.println("row:"+msgmatrix.length+"column:"+msgmatrix[0].length);
         for (int i = 0; i < msgmatrix[0].length; i++) {
             for (int j = 0; j < msgmatrix.length; j++) {
                 msgmatrix[j][i] = (msg.charAt(index++)-97)%26;
@@ -64,7 +75,7 @@ public class HillCipher {
     
     public static void genKeyMatrix() {
         keymatrix = new int[n][n];
-        inversmatrix = new int[n][n]
+        inversmatrix = new int[n][n];
         key = key.toLowerCase();
         int index=0;
 
@@ -143,13 +154,41 @@ public class HillCipher {
         }
     }
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n=2;
-        String msg = "POH";
-        init(msg,"GYBNQKURP");
-        System.out.println(encrypt());
-        System.out.println(decrypt());
-        
+        Scanner sc = new Scanner(System.in);        
+        printbanner();
+        String options="Hill Cipher\n----------------\n1.Encryption\n2.Decryption\n3.Exit\n",text,key;        
+        int choice;
+         do {
+            //Get the choice for the user
+            System.out.println(options+"Enter your Choice: ");
+            choice = sc.nextInt();
+            switch (choice) {
+                case 1:case 2:
+                    System.out.println("Enter the "+(choice==1?"plain":"cipher")+" text: ");
+                    msg=sc.next();
+                    if(msg.matches("[a-zA-Z]+")) {
+                        System.out.println("Enter the key: ");
+                        key=sc.next();
+                        if(key.matches("[a-zA-Z]+")) {
+                            init(msg, key);
+                            if(choice==1) 
+                                System.out.println(encrypt());
+                            else
+                                System.out.println(decrypt());
+                        }
+                        else
+                            System.out.println("Sorry,input mus be a word");
+                    }else
+                        System.out.println("Sorry,input must be a word");    
+                    break;
+                case 3:
+                    System.out.println("Bye..");
+                    break;
+                default:
+                    System.out.println("Invalid choice");
+                    break;
+            }
+        }while(choice!=3);
 
 
 
