8c38652d78759049765183fb7e1440f8a2dd4816
Manikumar-T <kumarmani862003@gmail.com>
2023-04-13 15:40:08 +0530

    update HillCipher.java fix the loop issue

--- Cryptography/Lab/Ex6_HillCipher/HillCipher.java
+++ Cryptography/Lab/Ex6_HillCipher/HillCipher.java
@@ -15,41 +15,56 @@ public class HillCipher {
     static int keymatrix[][],inversmatrix[][],msgmatrix[][],n,resultmatrix[][];
     static String key,msg;
     //method to find the K invers for the matrix either 2x2 or 3x3
-    public static void kInvers() {
+    public static boolean kInvers() {
         int detofk;
         if (keymatrix.length==2) {
             detofk= keymatrix[0][0]*keymatrix[1][1]-keymatrix[0][1]*keymatrix[1][0];
         }else {
-            detofk= keymatrix[0][0]*(keymatrix[1][1]*keymatrix[2][2]-keymatrix[1][2]*keymatrix[2][1])
-                    -keymatrix[0][1]*(keymatrix[1][0]*keymatrix[2][2]-keymatrix[2][0]*keymatrix[1][2])
-                    +keymatrix[0][2]*(keymatrix[1][0]*keymatrix[2][1]-keymatrix[1][1]*keymatrix[2][0]);
+            detofk= keymatrix[0][0]*((keymatrix[1][1]*keymatrix[2][2])-(keymatrix[1][2]*keymatrix[2][1]))
+                    -keymatrix[0][1]*((keymatrix[1][0]*keymatrix[2][2])-(keymatrix[2][0]*keymatrix[1][2]))
+                    +keymatrix[0][2]*((keymatrix[1][0]*keymatrix[2][1])-(keymatrix[1][1]*keymatrix[2][0]));
         }
+        detofk=detofk%26;
         detofk=detofk<0?detofk+26 : detofk;
+        System.out.println("det of k: "+detofk);
         int i=1;
-        while ((detofk*i++)%26!=1);
-        detofk=i-1;
-        if (keymatrix.length==2) {
-            inversmatrix[0][0]=keymatrix[1][1];
-            inversmatrix[0][1]=-keymatrix[0][1];
-            inversmatrix[1][0]=-keymatrix[1][0];
-            inversmatrix[1][1]=keymatrix[0][0];
-        }else {
-           inversmatrix[0][0] = +((keymatrix[1][1]*keymatrix[2][2])-(keymatrix[1][2]*keymatrix[2][1]));
-           inversmatrix[0][1] = -((keymatrix[0][1]*keymatrix[2][2])-(keymatrix[0][2]*keymatrix[2][1]));
-           inversmatrix[0][2] = +((keymatrix[0][1]*keymatrix[1][2])-(keymatrix[0][2]*keymatrix[1][1]));
-           inversmatrix[1][0] = -((keymatrix[1][0]*keymatrix[2][2])-(keymatrix[1][2]*keymatrix[2][0]));
-           inversmatrix[1][1] = +((keymatrix[0][0]*keymatrix[2][2])-(keymatrix[0][2]*keymatrix[2][0]));
-           inversmatrix[1][2] = -((keymatrix[0][0]*keymatrix[1][2])-(keymatrix[0][2]*keymatrix[1][0]));
-           inversmatrix[2][0] = +((keymatrix[1][0]*keymatrix[2][1])-(keymatrix[1][1]*keymatrix[2][0]));
-           inversmatrix[2][1] = -((keymatrix[0][0]*keymatrix[2][1])-(keymatrix[0][1]*keymatrix[2][0]));
-           inversmatrix[2][2] = +((keymatrix[0][0]*keymatrix[1][1])-(keymatrix[0][1]*keymatrix[1][0]));
-
-        }
-        for (i = 0; i < keymatrix.length; i++) {
-            for (int j = 0; j < keymatrix.length; j++) {
-                inversmatrix[i][j]=inversmatrix[i][j]<0?((inversmatrix[i][j]*detofk)%26)+26:(inversmatrix[i][j]*detofk)%26;
+        //check the detofk is even to not
+        if (detofk%2!=0) {
+            while ((detofk*i++)%26!=1);
+            detofk=i-1;
+            System.out.println(detofk);
+            if (keymatrix.length==2) {
+                inversmatrix[0][0]=keymatrix[1][1];
+                inversmatrix[0][1]=-keymatrix[0][1];
+                inversmatrix[1][0]=-keymatrix[1][0];
+                inversmatrix[1][1]=keymatrix[0][0];
+            }else {
+               inversmatrix[0][0] = +((keymatrix[1][1]*keymatrix[2][2])-(keymatrix[1][2]*keymatrix[2][1]));
+               inversmatrix[0][1] = -((keymatrix[0][1]*keymatrix[2][2])-(keymatrix[0][2]*keymatrix[2][1]));
+               inversmatrix[0][2] = +((keymatrix[0][1]*keymatrix[1][2])-(keymatrix[0][2]*keymatrix[1][1]));
+               inversmatrix[1][0] = -((keymatrix[1][0]*keymatrix[2][2])-(keymatrix[1][2]*keymatrix[2][0]));
+               inversmatrix[1][1] = +((keymatrix[0][0]*keymatrix[2][2])-(keymatrix[0][2]*keymatrix[2][0]));
+               inversmatrix[1][2] = -((keymatrix[0][0]*keymatrix[1][2])-(keymatrix[0][2]*keymatrix[1][0]));
+               inversmatrix[2][0] = +((keymatrix[1][0]*keymatrix[2][1])-(keymatrix[1][1]*keymatrix[2][0]));
+               inversmatrix[2][1] = -((keymatrix[0][0]*keymatrix[2][1])-(keymatrix[0][1]*keymatrix[2][0]));
+               inversmatrix[2][2] = +((keymatrix[0][0]*keymatrix[1][1])-(keymatrix[0][1]*keymatrix[1][0]));
+    
+            }//multiplay the adj with 1/k
+            for (i = 0; i < keymatrix.length; i++) {
+                for (int j = 0; j < keymatrix.length; j++) {
+                    // inversmatrix[i][j]=inversmatrix[i][j]<0?inversmatrix[i][j]+26:inversmatrix[i][j]%26;
+                    // inversmatrix[i][j]=(inversmatrix[i][j]*detofk)%26;
+                    inversmatrix[i][j]=inversmatrix[i][j]<0?((inversmatrix[i][j]*detofk)%26)+26:(inversmatrix[i][j]*detofk)%26;
+                }
             }
         }
+        else {
+            System.out.println("det of k is not possible");
+            return false;
+        }
+        return true;
+            
+       
     }
     //method to print the multidimensional matrix
     public static void printMatrix(int matrix[][]) {
@@ -118,7 +133,7 @@ public class HillCipher {
         genKeyMatrix();
         System.out.println("Key Matrix:");
         printMatrix(keymatrix);
-        kInvers();
+        if(kInvers()) {
         System.out.println("Invers Matrix:");
         printMatrix(inversmatrix);
         int sum=0;
@@ -139,17 +154,25 @@ public class HillCipher {
             }
         }
         return plaintext;
+        }
+        return "Invers matrix";
     }
     //inti method to initalize the matrix variable based on the square matrix condition
     public static boolean init(String msgloc,String keyloc) {
-        if(msgloc.length()%2==0 && keyloc.length()%2==0 && keyloc.length()/2 == 2 || keyloc.length()/2 == 3) {
+        if(keyloc.length()%2==0 && keyloc.length()/2 == 2 || keyloc.length()/2 == 3) {
             n=2;
+            if (msgloc.length()%2==1) {
+                 msgloc+="Z";
+            }
             msgmatrix = new int[2][msgloc.length()/2];
             resultmatrix = new int[2][msgloc.length()/2];
             msg=msgloc;
             key=keyloc;
-        }else if(msgloc.length()%3==0 && keyloc.length()%3==0 && keyloc.length()/3 == 2 || keyloc.length()/3 == 3) { 
+        }else if(keyloc.length()%3==0 && keyloc.length()/3 == 2 || keyloc.length()/3 == 3) { 
             n=3;
+            if (msgloc.length()%3==1) {
+                msgloc+="Z";
+           }
             msgmatrix  = new int[3][msgloc.length()/3];
             resultmatrix  = new int[3][msgloc.length()/3];
             msg=msgloc;
@@ -182,7 +205,8 @@ public class HillCipher {
                                 System.out.println("Cipher Text: "+encrypt());
                             else
                                 System.out.println("Plain Text: "+decrypt());
-                            }else
+                            }
+                            else
                                 System.out.println("Sorry,Square Matrix is not able to Generate");
                            
                         }
@@ -199,11 +223,6 @@ public class HillCipher {
                     break;
             }
         }while(choice!=3);
-
-
-
-
-
 
         {// int row  = sc.nextInt(),col = sc.nextInt();
         // keymatrix = new int[row][col];
